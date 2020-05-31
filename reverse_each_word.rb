0eea12a7ec91c44c2d3cd09fc023ccaf76cccfb4
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-31 17:04:21 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,20 @@
+
 def reverse_each_word(string)
-    new_array = [] 
-    array = string.split
-    new_array = array.collect {|i| i.reverse}
-    new_array.join(" ")  
-  end
+  new_array = [] 
+  array = string.split
+  new_array = array.collect {|i| i.reverse}
+  new_array.join(" ")  
+  
+end
+
+
+# def reverse_each_word(words)
+#   word = []
+  
+#   word << words.split(' ')
+#   word.each do |opposit|
+#     puts opposit.reverse()
+#   end 
+# end
+  
+
