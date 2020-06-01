5e8179dd3050264ec0235583520bcf4416a519eb
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2020-06-01 11:12:18 -0400

    Done.

--- spec/reverse_each_word_spec.rb
+++ spec/reverse_each_word_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe '#reverse_each_word' do
-  let(:sentence1) { "Hello there, and how are you?" }
+  let(:sentence1) {"Hello there, and how are you?"}
   it 'reverses all the words in a string without reversing the order of the words' do
     expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
   end
